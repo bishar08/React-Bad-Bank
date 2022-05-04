b0bf78c1dab4b53f830e4a1953c72859c1212da0
Bishar Mohamed <69430722+bishar08@users.noreply.github.com>
2022-05-03 23:58:01 -0500

    Update RenderContact.js

--- src/components/Contact/RenderContact.js
+++ src/components/Contact/RenderContact.js
@@ -2,7 +2,7 @@ import contact from './contactData'
 import './RenderContact.css'
 
 export default function RenderContact() {
-  let user = contact.users.find((el) => el.userId === 1)
+  let user = contact.users.find((el) => el.userId === 0)
   return (
     <>
       <li className="nav-item">
